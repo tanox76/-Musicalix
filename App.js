c3addd816a1b31e11ce168f51f7ac7266531fad0
Anton Oceanu <tanox76@yahoo.com>
2021-01-07 23:25:31 +0000

    third commit

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default function App(){
+   return <Screen> 
+     <Icon name = "email" size = {50} backgroundColor = "red" iconColor = "white"/>
+   </Screen>
+   ;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
